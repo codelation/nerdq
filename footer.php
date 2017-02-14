645d842ace76bdec70320ce497ccd8da2bee89b6
Josh Christy <josh@codelation.com>
2017-02-13 22:07:31 -0600

    adding analytics

--- footer.php
+++ footer.php
@@ -26,22 +26,13 @@
 		</div>
 	</footer>
 	<?php wp_footer(); ?>
-	<script type="text/javascript">
-		var today = new Date();
-		var launchDate = new Date('2017-01-01T05:59:59');
-		var timeLeft =  ((launchDate - today) / 1000).toFixed(0);
-
-		console.log(timeLeft);
-
-		if(timeLeft > 0) {
-			var clock = new FlipClock($('.countdown-to-launch'), {
-				autoStart: false,
-				countdown: true
-			});
-
-			clock.setTime(timeLeft);
-			clock.start();
-		}
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+	  ga('create', 'UA-91920516-1', 'auto');
+	  ga('send', 'pageview');
 	</script>
 </body>
 </html>
